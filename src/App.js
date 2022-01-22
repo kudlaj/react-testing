42af147ffd3044ef7d41d3a557fd3582fd064300
Jurgen <kudlaj@gmail.com>
2022-01-22 13:02:27 +0100

    init

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Testing demo</h1>
+      <p>Demo</p>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Testing demo/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Test', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Testing demo/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('should fail', () => {
+    throw new Error('Not yet implemented');
+  });
 });
+
