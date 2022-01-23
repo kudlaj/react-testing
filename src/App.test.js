d8bc4f7fb82532149c836aeba1df1b3cf8f106a7
Jurgen <kudlaj@gmail.com>
2022-01-23 18:03:03 +0100

    initial

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App Test', () => {
-  test('renders learn react link', () => {
+  test('should have the title: "Testing demo"', () => {
     render(<App />);
     const linkElement = screen.getByText(/Testing demo/i);
     expect(linkElement).toBeInTheDocument();
   });
 
-  test('should fail', () => {
+  test('should have a button', () => {
     throw new Error('Not yet implemented');
   });
+
+  test('should have a button with the initial text: "Turn to red" and the color blue', () => {
+    throw new Error('Not yet implemented');
+  });
+
+  test('should have a button that turns to red when clicked and change the text to', () => {
+    throw new Error('Not yet implemented');
+  });
+
+
 });
 
