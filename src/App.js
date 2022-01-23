bdb4333e1919545d1e7c5a2516be3bbc558b698c
Jurgen <kudlaj@gmail.com>
2022-01-23 12:43:58 +0100

    http mock

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ import MoviesList from './components/MoviesList';
 
 export const API = {
   getMovies() {
-    console.log('************ getMovies 1 ************');
       return fetch('https://swapi.dev/api/films/')
       .then(res => res.json())
   }
 }
 
 function App() {
-  console.log('************ App ************');
   const [movies, setMovies] = useState([]);
-
   const fetchMoviesHandler = () => {
-    console.log('fetchMoviesHandler');
     API.getMovies().then(data => {
-      console.log(data);
       setMovies(data.results.map(movie => {
         return {
           id: movie.episode_id,
